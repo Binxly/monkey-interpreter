ce59849d26704c222adb0c90bd75e9b1a262d411
Zac <contact@binx.page>
2025-01-30 15:38:05 -0500

    initial commit

---
+++ token/token.go
@@ -0,0 +1,36 @@
+// token/token.go
+
+package token
+
+type TokenType string
+
+type Token struct {
+	Type	TokenType 	// TokenType = string = many possible values for TokenType = distinguishing between types of tokens
+	Literal string 		// may not perform same as int/byte, but simpler
+}
+
+const (
+	ILLEGAL = "ILLEGAL" 	// token/char we don't know about
+	EOF	= "EOF"		// "end of file" -> parser can stop
+
+	// Identifiers + literals
+	IDENT 	= "IDENT" 	// add, foobar, x, y...
+	INT 	= "INT" 	// 1343456 
+
+	// Operators
+	ASSIGN	= "="
+	PLUS 	= "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
