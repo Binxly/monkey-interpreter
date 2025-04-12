4f8e00f9410c8093fc518c8a6ec8a1943043290d
Zac <zbagley610@gmail.com>
2025-04-12 08:39:08 -0400

    Added parser errors to REPL... and a monkey :)

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -23,9 +23,40 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+const MONKEY_AROUND = `
+            ,.-" "-.,
+           /   ===   \
+          /  =======  \
+       __|  (o)   (0)  |__      
+      / _|    .---.    |_ \         
+     | /.----/ O O \----.\ |       
+      \/     |     |     \/        
+      |                   |            
+      |                   |           
+      |                   |          
+      _\   -.,_____,.-   /_         
+  ,.-"  "-.,_________,.-"  "-.,
+	`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_AROUND+"\n")
+	io.WriteString(out, "Well this is embarrassing... We ran into some monkey business\n")
+	io.WriteString(out, " parser errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
